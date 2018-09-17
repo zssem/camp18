3edfece4c0e3616610f14d4bf1d6545e47bc822d
psddingo <psddingo@scrum.org>
2018-09-17 16:11:24 +0200

    configurable endpoint urls

--- src/main/java/com/zuehlke/movieticketservice/MovieTicketServiceApplication.java
+++ src/main/java/com/zuehlke/movieticketservice/MovieTicketServiceApplication.java
@@ -2,6 +2,7 @@ package com.zuehlke.movieticketservice;
 
 import com.zuehlke.movieticketservice.movie.movieservice.MovieServiceAdapter;
 import com.zuehlke.movieticketservice.movie.rating.RatingAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,13 +15,13 @@ public class MovieTicketServiceApplication {
 	}
 
 	@Bean
-	public MovieServiceAdapter movieServiceAdapter() {
-		return new MovieServiceAdapter("https://movie-service.herokuapp.com/");
+	public MovieServiceAdapter movieServiceAdapter(@Value("${endpoint.movie-service}") String urlMovieService) {
+		return new MovieServiceAdapter(urlMovieService);
 	}
 
 	@Bean
-	public RatingAdapter ratingAdapter() {
-		return new RatingAdapter("https://movie-rating-service.herokuapp.com/");
+	public RatingAdapter ratingAdapter(@Value("${endpoint.movie-rating-service}") String urlMovieRatingService) {
+		return new RatingAdapter(urlMovieRatingService);
 	}
 
 }
