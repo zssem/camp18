e8aa0e9276a9e9f975dc4287057cd637d27a1fdc
psddingo <psddingo@scrum.org>
2018-09-17 11:57:42 +0200

    MovieControllerTest

--- src/test/java/com/zuehlke/movieticketservice/MovieControllerTest.java
+++ src/test/java/com/zuehlke/movieticketservice/MovieControllerTest.java
@@ -1,6 +1,7 @@
 package com.zuehlke.movieticketservice;
 
 import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +9,9 @@ import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MovieControllerTest {
@@ -23,11 +26,21 @@ public class MovieControllerTest {
 
     @Test
     public void getMovies() throws Exception {
-
+        when()
+                .get("/api/v1/movies")
+                .then()
+                .statusCode(200)
+                .body("[0].id", equalTo(1))
+                .body("[1].id", Matchers.equalTo(2))
+                .body("[2].id", Matchers.equalTo(3));
     }
 
     @Test
     public void getMovieById() throws Exception {
-
+        when()
+                .get("/api/v1/movies/1")
+                .then()
+                .statusCode(200)
+                .body("title", equalTo("Batman Begins"));
     }
 }
